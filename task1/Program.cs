f520cef7d697c4a0fa775b6764e5a1589b5c3bde
Kirill P <Kirillural555@gmail.com>
2024-03-11 10:15:04 +0300

    2 task

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача первая, проверка на кратность числа 7 & 23 одновременно это число 161
+using System;   // оператор позволяющий использовать ссылки на другие типы данных
+class Program 
+{
+    static void Main()
+    {
+        Console.Write("Введите число ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else 
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
